ffa1faa365ec71a0453e9650c7808e358ee77b22
Shane Curtis <c84a@ymail.com>
2021-10-13 19:52:44 +0800

    Completed about page styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,20 +1,17 @@
-
-import React from 'react';
+import React from "react";
+import Image from '../../assets/images/profile.png'
+import '../../styles/About.css'
 
 export default function About() {
-    return (
+  return (
+    <div className="about-body">
+      <h1 className="page-header">About Me</h1>
 
-       <div>
-
-           <h2>About Me</h2>
-
-           <p></p>
-
-
-
-       </div>
-
-
-
-    );
+      <p className="about-content">
+        Hi am Software Developer that has recently completed a Bootcamp in FullStack Web Development at the University of Western Australia. I love all things Tech and have a
+         background in IT Tech Support. I am contactable by Email, Github and Linked in. Please feel free to reach out at the links provided below. 
+      </p>
+      <img id="profile" src={Image} alt="Profile Pic"></img>
+    </div>
+  );
 }
